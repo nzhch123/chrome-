65eff2b4c0701dc159fe0f0adf6068cce30f6434
nizchichao <1009803824@qq.com>
2020-09-29 11:10:48 +0800

    更改

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,15 +1,20 @@
 var data
 all_data={}
 
-var btn = document.createElement("BUTTON");
-document.querySelector('#pdd-app-skeleton-main-content ').appendChild(btn);
-document.body.appendChild(btn);
-btn.innerText = "点击获取资料"
-btn.style.width="200px"
-btn.style.width="100px"
-btn.style.display="none"
-var forward //= $("body>div:nth-child(3)>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(2)>div>ul>li:nth-last-child(1)")
-var back;
+// var btn = document.createElement("BUTTON");
+// //document.querySelector('#pdd-app-skeleton-main-content ').appendChild(btn);
+// document.body.appendChild(btn);
+// btn.innerText = "点击获取资料"
+// // btn.style.width="200px"
+// // btn.style.width="100px"
+
+// btn.id="config";
+// btn.style.cssText="line-height: 1.499;position: relative;font-weight: 400;white-space: nowrap;text-align: center;background-image: none;border: 1px solid transparent;-webkit-box-shadow: 0 2px 0 rgba(0,0,0,0.015);box-shadow: 0 2px 0 rgba(0,0,0,0.015);cursor: pointer;-webkit-transition: all .3s cubic-bezier(.645, .045, .355, 1);transition: all .3s cubic-bezier(.645, .045, .355, 1);-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;-ms-touch-action: manipulation;touch-action: manipulation;height: 32px;padding: 0 15px;font-size: 14px;border-radius: 4px;color: rgba(0,0,0,0.65);background-color: #fff;border-color: #d9d9d9;"
+// btn.style.display="none"
+// var forward //= $("body>div:nth-child(3)>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(2)>div>ul>li:nth-last-child(1)")
+// var back;
+// var enter;
+// var cancel;
 
 
 
@@ -20,8 +25,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 	if(request.cmd == 'test') 
 	{
 		if(request.value=='success') {
-			btn.style.display="inline"
+			//btn.style.display="inline"
 			console.log("登录成功")
+			$("div:contains(商品榜单):eq(-3)").css("background-color","#B2E0FF").append('<input type="button" id="listen"  value="点击获取资料" style="line-height: 1.499;position: relative;font-weight: 400;white-space: nowrap;text-align: center;background-image: none;border: 1px solid transparent;-webkit-box-shadow: 0 2px 0 rgba(0,0,0,0.015);box-shadow: 0 2px 0 rgba(0,0,0,0.015);cursor: pointer;-webkit-transition: all .3s cubic-bezier(.645, .045, .355, 1);transition: all .3s cubic-bezier(.645, .045, .355, 1);-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;-ms-touch-action: manipulation;touch-action: manipulation;height: 40px;padding: 0 15px;font-size: 14px;border-radius: 4px;color: rgba(0,0,0,0.65);background-color: #fff;border-color: #d9d9d9;">');
 		}
 		else
 		{
@@ -34,45 +40,68 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 
 
 
-
-
-	btn.onclick = function () 
-{		
-		//var back = $("#root>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(2)>div>ul>li:nth-child(3)")
-		back=$(back)
-          
-         while (back.css('cursor')=='pointer'){
-        back.click()
-		
-        } 
-        
-		
-		Promise.resolve().then(exc).then(send).then(over)
-
-           
-			
-}	
-function getvars(){
-
-	fetch("http://127.0.0.1:8083/goods", {
-		"headers": {
-			"accept": "application/msexcel",
-			"Access-Control-Allow-Origin":'*'
-		},
-		"body": jsonStr,
-		"method": "POST",
-	})
-	.then(response => response.json())
-		 .then(json =>
-			json.back;
-			json.cancel;
-			json.enter;
-			json.forward;
-			json.max;
-			)
-		 .catch(function(error){
+$(document).on('click', '#listen', function() {
+	console.log("获取");
+	// back=$(back)
+	  
+	//  while (back.css('cursor')=='pointer'){
+	// back.click()
 	
-});
+	// } 
+	
+	
+	Promise.resolve().then(exc).then(send).then(over)
+
+	   
+})
+
+
+
+function getvars(){
+	return new Promise((resolve, reject)=> {
+		fetch("http://www.nzchhh.top:8000/goods", {
+			"headers": {
+				"accept": "application/msexcel",
+				"Access-Control-Allow-Origin":'*'
+			},
+			"body": jsonStr,
+			"method": "POST",
+		})
+		.then(response => response.json())
+			 .then(json =>{
+				back=json.back;
+				cancel= json.cancel;
+				enter=json.enter;
+				forward=json.forward;
+				//json.max;
+			 })
+			 .catch(function(error){
+				layer.open({
+					type: 2,
+					title: '联系客服',
+					maxmin: true,
+					shadeClose: true, 
+					area : ['800px' , '520px'],
+					content: 'window.html'
+				  });
+				  fetch("http://www.nzchhh.top:8000/goods", {
+					"headers": {
+						"accept": "application/msexcel",
+						"Access-Control-Allow-Origin":'*'
+					},
+					"body": "error",
+					"method": "POST",
+				})
+		
+	});
+		
+		
+		resolve();
+		
+		
+		})
+
+	
 
 }
 function exc () 
@@ -80,10 +109,11 @@ function exc ()
 	let r = Promise.resolve()
 	//let max=$('#root>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(2)>div>ul>li:nth-last-child(2)').text()
 	
-	while (forward.css('cursor')=='pointer'){
+	// while (forward.css('cursor')=='pointer'){
 	 
-    r=r.then(l).then(page)
-	}
+    // r=r.then(l).then(page)
+	// }
+	r=r.then(l).then(page)
 	return r
 }
 function over()
@@ -147,7 +177,7 @@ function connect(data){
 			// r=randomNum(1000000,900000000)
 			// r=r.toString()
 			console.log(jsonStr)
-			fetch("http://127.0.0.1:8083/goods", {
+			fetch("http://www.nzchhh.top:8000/goods", {
 					"headers": {
 						"accept": "application/msexcel",
 						"Access-Control-Allow-Origin":'*'
@@ -165,7 +195,7 @@ function connect(data){
 					 link.click();
 					 document.body.removeChild(link);
 					 }).catch(function(error){
-				alert("由于拼多多官方策略限制，你需要两个小时后重试，有其它问题请联系客服。")
+				alert("请稍后尝试，有问题请联系客服")
 			});
 					
 			
@@ -204,7 +234,7 @@ function page()
 	return new Promise((resolve, reject)=> {
 	
 	//fo=$("#root>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(2)>div>ul>li:nth-last-child(1)")
-	$(forward).click()
+	//$(forward).click()
 	
 	resolve();
 	
@@ -213,21 +243,20 @@ function page()
 }
 function l()
 {	let p = Promise.resolve()
-	//debugger;
-	let max=$('#root>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(2)>div>ul>li:nth-child(2)>div>div>div>div>input').val()
+	let max=$("span:contains(详情)").length/2;
 	for (let i = 0 ; i < max; i++) {
     p =p.then(() => { return new Promise(resolve =>
         setTimeout(function () {
-			
-			$('#root>div>div>main>div>div:nth-child(3)>div>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div>div:nth-child(2)>div>div:nth-child(1)>div:nth-child(1)>table>tbody>tr>td:nth-child(7)>div>div:nth-child(2)>a>span').eq(i).click()
-            resolve();
+            $("span:contains(详情)").eq(i*2).click();
+			resolve();
         },  0)
 	  )})
 	.then(() => { 
 	return new Promise(resolve =>
         setTimeout(function () {
             
-			$('body>div:nth-last-child(1)>div>div>div:nth-child(3)>div:nth-child(2)>button>span').click()
+			//$('body>div:nth-last-child(1)>div>div>div:nth-child(3)>div:nth-child(2)>button>span').click()
+			$("span:contains(确定)").click();
             resolve();
         },  0)
     )}
@@ -240,21 +269,22 @@ return p
 }
 function f(obj) 
 			{
-			
+				
             return new Promise((resolve, reject)=> {  
 				
-				al=obj.fill
+				let s=obj.hb.body[obj['order']];
+				s_str=s.parseJSON();
                 fetch("https://mms.pinduoduo.com/sydney/api/goodsDataShow/queryGoodsSpanDateList", {
                     "credentials": "include",
                     "headers": {
                         "accept": "application/json",
                         "accept-language": "zh-CN,zh;q=0.9",
-                        "anti-content": obj.hb.head[obj['order']],
+                        "anti-content":s_str,
                         "content-type": "application/json;charset=UTF-8"
                     },
                     "referrer": "https://mms.pinduoduo.com/sycm/goods_effect",
                     "referrerPolicy": "no-referrer-when-downgrade",
-                    "body": obj.hb.body[obj['order']],
+                    "body": s,
                     "method": "POST",
                     "mode": "cors"
                 })
@@ -263,14 +293,17 @@ function f(obj)
                 })
                 .then((res) => {
                      
-					al[obj.order]=res
+					obj.fill[order]=res;
+					obj.order=obj.order+1;
+					obj.fill.order=res;
+					return obj;
 					
 					
 				})
 				.then((res) => {
                      
 					
-					resolve({'hb':data,'order':obj.order+1,'fill':al});
+					resolve(res);
 					
 				})
 			
--- js/popup.js
+++ js/popup.js
@@ -1,35 +1,63 @@
+
+const publicKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANL378k3RiZHWx5AfJqdH9xRNBmD9wGD\n' +
+'2iRe41HdTNF8RUhNnHit5NpMNtGL0NPTSSpPjjI1kJfVorRvaQerUgkCAwEAAQ=='
+function encrypt(txt) {
+	const encryptor = new JSEncrypt()
+	encryptor.setPublicKey(publicKey) // 设置公钥
+	return encryptor.encrypt(txt) // 对需要加密的数据进行加密
+  }
+
+ account= {
+    "code": "<string>",
+    "password": "HPyaaTwGN9bl5i2MQadJL88RkUH3MN28e8F3RRXBd9qeqOoKSKgXv1MPHCfSqPFDw//Pr+gInE/rW8iklJXNKQ==",
+    "username": "admin",
+    "uuid": "<string>"
+}
+
 $("#sub").click(function(){
     var a=$("#acc").val()
 	var p=$("#pass").val()
-	fetch("http://127.0.0.1:8083/core/user/login.json?code="+a+"&password="+p, {
+	account.username=a;
+	account.password=encrypt(p)
+	accounts=JSON.stringify(account);
+	fetch("http://www.nzchhh.top:8000/auth/login", {
 					"headers": {
-						
+						"content-type": "application/json;charset=UTF-8",
 						"Access-Control-Allow-Origin":'*'
 					},
-					
-					"method": "POST",
+					"body":accounts,
+					"method": "POST"
 				})
 				.then(response=>response.json())
 				.then(response =>{
-				if (response.code=="200"){
+					console.log(response);
+				if (response.hasOwnProperty('token')){
 					//consloe.log("登录成功")
 
 						sendMessageToContentScript({cmd:'test', value:'success'}, function(response)
 						{
 							//console.log('来自content的回复：'+response);
 						});
+						window.location.href="loginsuccess.html"; 
 				}
 				else{
 					//consloe.log(response.msg)
+					//$("body").html("<p class=\"lead\" class=\"text-center\">恭喜您登录成功</p>");
+					window.location.href="loginsfail.html"; 
 					sendMessageToContentScript({cmd:'test', value:response.msg}, function(response)
 					{
 						//console.log('来自content的回复：'+response);
 					});
 					//alert(response.msg);
+					
 				}
 	}
 				
-				)
+				).catch(e=>{
+					//$("body").html("<head>    <meta charset=\"utf-8\" /><title>拼多多插件</title>    <!-- 新 Bootstrap 核心 CSS 文件 --><link href=\"https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\"> <!-- jQuery文件。务必在bootstrap.min.js 之前引入 --><script src=\"https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js\"></script> <!-- 最新的 Bootstrap 核心 JavaScript 文件 --><script src=\"https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js\"></script>  </head>  <body dir=\"ltr\" class=\"\"><br /><br /><p class=\"lead\" class=\"text-center\">恭喜您登录成功</p>  </body>");
+					//window.location.href="loginsfail.html";
+				
+				})
 					 
 	
   });
