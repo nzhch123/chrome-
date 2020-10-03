ad075eeaa30a3774c85725a6d06d819562b007f9
nizchichao <1009803824@qq.com>
2020-10-03 20:30:39 +0800

    拦截

--- js/injected-script.js
+++ js/injected-script.js
@@ -1,38 +1,46 @@
 flag=0;
 order=0;
 all_data={}
-window.addEventListener("message", function(e)
-{   if (e.origin=="https://mms.pinduoduo.com"){
-        if (e.data=="start"){
-            flag=1;
-            order=1;
-        }
-        if (e.data=="finish"){
-            flag=0;
-            order=0;
-            console.log(all)
-            window.postMessage(all_data, '*');
-        }
-
-}
-
-}, false);
 let fetch_kinds = {
 
 
     originalFetch: window.fetch.bind(window),
     myFetch: function(...args) {
         return fetch_kinds.originalFetch(...args).then((response) => {
-            return response.json();
-        }).then((response) => {console.log(response)});
+            if (response.url=="https://mms.pinduoduo.com/sydney/api/goodsDataShow/queryGoodsSpanDateList")
+            {
+                return response.json();
+            }
+
+        }).then((response) => {
+            //console.log(response)
+            if (response!=null){
+                all_data[order]=response;
+                order++;
+            }
+
+        });
     },
 }
+window.addEventListener("message", function(e)
+{   if (e.origin=="https://mms.pinduoduo.com"){
+        if (e.data=="start"){
+            flag=1;
+            order=0;
+            window.fetch = fetch_kinds.myFetch;
+        }
+        if (e.data=="finish"){
+            flag=0;
+            order=0;
+            window.fetch = fetch_kinds.originalFetch;
+            console.log(all_data)
+            all_data={}
+           // window.postMessage(all_data, '*');
+        }
 
-
-if (fetch_kinds.settings.ajaxInterceptor_switchOn) {
-    window.XMLHttpRequest = fetch_kinds.myXHR;
-    window.fetch = fetch_kinds.myFetch;
-} else {
-    window.XMLHttpRequest = fetch_kinds.originalXHR;
-    window.fetch = fetch_kinds.originalFetch;
 }
+
+}, false);
+
+
+
